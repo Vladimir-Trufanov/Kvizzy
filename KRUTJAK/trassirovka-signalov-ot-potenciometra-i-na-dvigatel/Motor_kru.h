2fe8b294d193d1a340abed1213104389e1357fea
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2024-03-25 13:32:35 +0300

    Создан класс драйвера мотора

---
+++ KRUTJAK/trassirovka-signalov-ot-potenciometra-i-na-dvigatel/Motor_kru.h
@@ -0,0 +1,99 @@
+//                                                          *** Motor_kru.h ***
+
+
+
+#ifndef Motor_kru
+#define Motor_kru
+ 
+#pragma once          // обеспечили разовое подключение файла
+#include <Arduino.h>  // подключили оьщие функции Arduino
+
+class MotorKrutjak 
+{
+   // Передача массива в класс
+   // Итог:сумма элементов массива
+   public:
+      int PinRes;       // аналоговый вход, к которому подключен потенциометр
+      int PinPWM_L;     // цифровой (ШИМ) выход, к которому подключено левое плечо транзисторов
+      int PinPWM_R;     // цифровой (ШИМ) выход, к которому подключено правое плечо
+      void Driver(int PinPWM_L, int PinPWM_R, int PinRes); 
+      void Init(int PinPWM_L, int PinPWM_R); 
+   private:
+};
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** Arduino UNO                                               *** Motor.cpp ***
+ * 
+ * Драйвер электромотора на с использованием широтно-импульсной модуляции, 
+ * двух n-канальных полевых транзисторов IRFZ24N и двух p-канальных полевых 
+ * транзисторов IRF9Z24N.
+
+ * 
+ * v1.1, 25.03.2024                                   Автор:      Труфанов В.Е.
+ * Copyright © 2024 tve                               Дата создания: 24.03.2024
+**/
+
+
+
+
+
+
+
+/*
+#ifndef Motor_cpp
+#define Motor_cpp
+*/
+
+/* Параметры, передаваемые драйверу 
+const int PinRes   = A0;     // аналоговый вход, к которому подключен потенциометр
+const int PinPWM_L = 10;     // цифровой (ШИМ) выход, к которому подключено левое плечо транзисторов
+const int PinPWM_R = 9;      // цифровой (ШИМ) выход, к которому подключено правое плечо
+*/
+
+/*
+#include "Arduino.h"
+
+void MotoriInit(int PinPWM_L, int PinPWM_R) 
+{
+   pinMode(PinPWM_L, OUTPUT); // назначили, как выход, контакт 10 Arduino 
+   pinMode(PinPWM_R, OUTPUT); 
+}
+
+void MotoriDriver(int PinPWM_L, int PinPWM_R, int PinRes) 
+{
+   int ValRes = 0;     // считанное значение напряжения с аналогового контакта (0-1023)
+   int ValPWM_L = 0;   // хранимое значение ШИМ для отправки на затворы левого плеча (0-255)
+   int ValPWM_R = 0;   // хранимое значение ШИМ для отправки на затворы правого плеча (0-255)
+  
+   ValRes = analogRead(PinRes);           // считали напряжение с потенциометра
+   if (ValRes<512)
+   {
+      ValPWM_L = map(ValRes, 0,512, 255,0);   // расчитали напряжение ШИМ
+      ValPWM_R = 0;   
+   }
+   else
+   {
+      ValPWM_R = map(ValRes, 512,1023, 0,255);   // расчитали напряжение ШИМ
+      ValPWM_L = 0;   
+   }
+   analogWrite(PinPWM_L,ValPWM_L);            // установили скорость мотора в дискретах
+   analogWrite(PinPWM_R,ValPWM_R);            // установили скорость мотора в дискретах
+}
+
+#endif
+*/
+// ************************************************************** Motor.cpp ***
--- KRUTJAK/trassirovka-signalov-ot-potenciometra-i-na-dvigatel/trassirovka-signalov-ot-potenciometra-i-na-dvigatel.ino
+++ KRUTJAK/trassirovka-signalov-ot-potenciometra-i-na-dvigatel/trassirovka-signalov-ot-potenciometra-i-na-dvigatel.ino
@@ -4,58 +4,42 @@
  * Изучить взаимосвязанные сигналы от потенциометра и на двигатель у драйвера
  * электромотора
  * 
- * v1.0, 24.03.2024                                   Автор:      Труфанов В.Е.
+ * v1.1, 25.03.2024                                   Автор:      Труфанов В.Е.
  * Copyright © 2024 tve                               Дата создания: 24.03.2024
 **/
 
-const int PinRes   = A0;     // аналоговый вход, к которому подключен потенциометр
-const int PinPWM_L = 10;     // цифровой (ШИМ) выход, к которому подключено левое плечо транзисторов
-const int PinPWM_R = 9;      // цифровой (ШИМ) выход, к которому подключено правое плечо
+#include "Motor_kru.h"    // подключили драйвер мотора
 
+const int PinRes   = A0;  // аналоговый вход, к которому подключен потенциометр
+const int PinPWM_L = 10;  // цифровой (ШИМ) выход, к которому подключено левое плечо транзисторов
+const int PinPWM_R = 9;   // цифровой (ШИМ) выход, к которому подключено правое плечо
+
+/*
 volatile int ValRes = 0;     // считанное значение напряжения с аналогового контакта (0-1023)
 volatile int ValPWM_L = 0;   // хранимое значение ШИМ для отправки на затворы левого плеча (0-255)
 volatile int ValPWM_R = 0;   // хранимое значение ШИМ для отправки на затворы правого плеча (0-255)
+*/
 
-#define LEDPIN 13
+#define LEDPIN 13            
+
 // Определяем счетчик прерываний от таймера и общее их количество до
 // события переключения светодиода  
 volatile unsigned int cntr;
 const unsigned int BtnToggle = 62499;
 
+// Инициируем драйвер мотора
+MotorKrutjak Motor; 
+
 void setup() 
 {
-   MotorInit();
+   Motor.Init(PinPWM_L,PinPWM_R);
    TrassInit();   
 }
 
 void loop() 
 {
-   MotorDriver();
+   Motor.Driver(PinPWM_L,PinPWM_R,PinRes);
    TrassMake();   
-}
-
-
-// Драйвер мотора
-void MotorInit() 
-{
-   pinMode(PinPWM_L, OUTPUT); // назначили, как выход, контакт 10 Arduino 
-   pinMode(PinPWM_R, OUTPUT); 
-}
-void MotorDriver() 
-{
-   ValRes = analogRead(PinRes);           // считали напряжение с потенциометра
-   if (ValRes<512)
-   {
-      ValPWM_L = map(ValRes, 0,512, 255,0);   // расчитали напряжение ШИМ
-      ValPWM_R = 0;   
-   }
-   else
-   {
-      ValPWM_R = map(ValRes, 512,1023, 0,255);   // расчитали напряжение ШИМ
-      ValPWM_L = 0;   
-   }
-   analogWrite(PinPWM_L,ValPWM_L);            // установили скорость мотора в дискретах
-   analogWrite(PinPWM_R,ValPWM_R);            // установили скорость мотора в дискретах
 }
 
 // Трассировка управления мотором
@@ -64,7 +48,6 @@ void TrassInit()
    pinMode(LEDPIN, OUTPUT);
    cntr=0;
    cli(); // отключили прерывания
-
    // Инициализируем регистры Timer2
    TCCR2A = 0; 
    TCCR2B = 0;
@@ -73,7 +56,6 @@ void TrassInit()
    TIMSK2 = (1 << TOIE2);
    // Устанавливаем CS10 бит так, чтобы таймер работал при тактовой частоте:
    TCCR2B |= (1 << CS20);
-
    sei(); // включили прерывания
 }
 
@@ -81,10 +63,8 @@ void TrassMake()
 {
    if (cntr>BtnToggle)
    {
-      //cli();
       digitalWrite(LEDPIN, !digitalRead(LEDPIN));
       cntr=0;
-      //sei();
    }
 }
 
@@ -92,6 +72,5 @@ ISR(TIMER2_OVF_vect)
 {
    cntr=cntr+1;
 }
-
 
 // **************** trassirovka-signalov-ot-potenciometra-i-na-dvigatel.ino ***
