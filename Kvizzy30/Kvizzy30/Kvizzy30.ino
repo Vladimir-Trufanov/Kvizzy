be64dabc597d16392556bedf5511c7157b278e11
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2024-10-26 10:20:31 +0300

    Опыты с JSON

--- Kvizzy30/Kvizzy30/Kvizzy30.ino
+++ Kvizzy30/Kvizzy30/Kvizzy30.ino
@@ -1,185 +1,71 @@
-/** Arduino C/C++ ******************************************** Kvizzy30.ino ***
- * 
- * Kvizzy - система контроллеров, датчиков и исполнительных устройств моего, 
- * стремящегося к умному, хозяйства.
+/** Arduino, Esp32-CAM *************************************** Kvizzy30.ino ***
  * 
  * Kvizzy30 - модельное программное обеспечение для Esp32-CAM и двух встроенных
  * светодиодов: контрольного и вспышки
  * 
- * v3.1, 07.10.2024                                    Автор:      Труфанов В.Е.
- * Copyright © 2024 tve                                Дата создания: 31.05.2024
+ * v3.1, 07.10.2024                                   Автор:      Труфанов В.Е.
+ * Copyright © 2024 tve                               Дата создания: 31.05.2024
+ * 
+ *           Kvizzy - система контроллеров, датчиков и исполнительных устройств 
+ *                    моего, стремящегося к умному, хозяйства (нижний уровень).
+ * 
 **/
 
-// 2024-07-20
-// 
-// Payment:                              "Al Thinker ESP32-CAM"
-// CPU Frequency:                        "240MHz (WiFi/BT)"
-// Flash Frequency:                      "80MHz"
-// Flash Mode:                           "QIO"
-// Partition Scheme:                     "Minimal SPIFFS (1.9MB APP with OTA/190KB SPIFFS)"
-// Core Debug Level:                     "Ничего"
-// Erase All Flash Before Sketch Upload: "Enabled"
-// Port:                                 "COM5"
-
-// Additional links for the Board Manager: https://espressif.github.io/arduino-esp32/package_esp32_dev_index.json
-
-// Менеджер плат:         esp32 by Espressif Systems 3.0.1 installed
-// Платы в данном пакете: ESP32 Dev Board, ESP32-S2 Dev Board, ESP32-S3 Dev Board, ESP32-C3 Dev Board, Arduino Nano ESP32.
-
-
-
-
-
-// Подключаем библиотеку для работы с HTTP-протоколом
-#include <WiFi.h>
-#include <HTTPClient.h>
-
-#include <tinyxml2.h>
-using namespace tinyxml2;
-
-// Вводим имя и пароль точки доступа
-//const char *ssid =     "linksystve";     // Put your SSID here
-//const char *password = "x93k6kq6wf";     // Put your PASSWORD here
-
-//const char* ssid     = "OPPO A9 2020";
-//const char* password = "b277a4ee84e8";
-
-const char* ssid     = "TP-Link_B394";
-const char* password = "18009217";
-
-int iloop=0;
+#include "define_kvizzy.h"     // подключили общие определения 
+#include "common_kvizzy.h"   // подключили общие функции  
 
 void setup() 
 {
-  Serial.begin(115200);
-  // делаем небольшую задержку на открытие монитора порта
-  delay(5000);
-  // подключаемся к Wi-Fi сети
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(1000);
-    Serial.println("Соединяемся с Wi-Fi..");
-  }
-  Serial.println("Соединение с Wi-Fi установлено");
+   Serial.begin(115200);
+   while (!Serial) continue;
+   String g=jison1();
+   Serial.println(g);
 
-  pinMode(4,OUTPUT);   // "вспышка"
-  pinMode(33,OUTPUT);    // "работает"
+   while (!Serial) continue;
+
+   ssetup(g);
 }
 
 void loop() 
 {
-  iloop=iloop+1;
-  Serial.print("iloop = ");
-  Serial.println(iloop);
-  
-  // выполняем проверку подключения к беспроводной сети
-  if ((WiFi.status() == WL_CONNECTED)) 
-  {
-    // создаем объект для работы с HTTP
-    HTTPClient http;
-    // подключаемся к веб-странице
-    //http.begin("https://doortry.ru/stihi/sorevnovanie-s-hakerami");
-    //http.begin("https://kwinflat.ru/Dacha/");
-    //http://kwinflatht.nichost.ru/
-
-    //String xmlname="https://kwinflat.ru/Dacha/Proba.xml";
-    //String xmlname="https://kwinflatht.nichost.ru/";
-    String xmlname="https://kwinflatht.nichost.ru/State/State.php";
-    http.begin(xmlname);
-    // делаем GET запрос
-    int httpCode = http.GET();
-    // проверяем успешность запроса
-    if (httpCode > 0) 
-    {
-      // выводим ответ сервера
-      String payload = http.getString();
-      Serial.println("httpCode ---");
-      Serial.println(httpCode);
-      Serial.println("payload ----");
-      Serial.println(payload);
-      Serial.println("------------");
-
-      /*
-      // Распознаем XML-файл
-      int length = payload.length();
-      char* testDocument = new char[length + 1];  
-      strcpy(testDocument,payload.c_str()); 
-
-      XMLDocument xmlDocument;
-      if (xmlDocument.Parse(testDocument)!= XML_SUCCESS)
-      {
-         Serial.print("Ошибка распознавания файла: ");
-         Serial.println(xmlname);
-         return; 
-      };
-
-      // Отмечаем успешное распознавание xml-файла
-      Serial.print(xmlname);
-      Serial.println(" - файл распознан!");
-
-      // Подключаем к разбору корневой тег 
-      XMLNode* root = xmlDocument.FirstChild();
-
-      // Выбираем значение параметра "sgpa"
-      Serial.println("sgpa -------");
-      XMLElement* sgpa = root->FirstChildElement("sgpa");
-      int val;
-      sgpa->QueryIntText(&val);
-      Serial.println(val);
-      Serial.println("------------");
-
-      // Выбираем значение параметра "name"
-      Serial.println("name -------");
-      XMLElement* text = root->FirstChildElement("name");
-      if (text != nullptr) Serial.println(text->GetText());
-      else Serial.println("nullptr");
-      Serial.println("------------");
-
-      // Выбираем значение параметра "noname"
-      Serial.println("noname -----");
-      XMLElement* noname = root->FirstChildElement("noname");
-      if (noname != nullptr) Serial.println(noname->GetText());
-      else Serial.println("nullptr");
-      Serial.println("------------");
-      */
-    }
-    else 
-    {
-      Serial.println("Ошибка HTTP-запроса");
-    }
-    // освобождаем ресурсы микроконтроллера
-    http.end();
-  }
-  delay(3000);
-
-
-  // 
-  Serial.println("Погасили");
-  digitalWrite(33,HIGH);  // рабочая
-  digitalWrite(4,LOW);    // "вспышка"
-  delay(1000);
-  view();
-  delay(4000);
-  Serial.println("Зажгли");
-  digitalWrite(33,LOW);   // рабочая
-  digitalWrite(4,HIGH);   // "вспышка"
-  delay(1000);
-  view();
-  delay(4000);
-  Serial.println("Погасили");
-  digitalWrite(33,HIGH);  // рабочая
-  digitalWrite(4,LOW);    // "вспышка"
-  delay(1000);
-  view();
-  delay(4000);
-
 }
 
-void view() 
+void ssetup(String g) 
 {
-  if (digitalRead(4) == HIGH) Serial.println("4-горит"); 
-  else Serial.println("4-потушена");
-  if (digitalRead(33) == HIGH) Serial.println("33-потушена"); 
-  else Serial.println("33-горит");
+
+  // Allocate the JSON document
+  JsonDocument doc;
+
+  // JSON input string.
+  const char* json = "12{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+  //const char* json = g.c_str();
+
+
+  // Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, json);
+
+  // Test if parsing succeeds.
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  // Fetch values.
+  //
+  // Most of the time, you can rely on the implicit casts.
+  // In other case, you can do doc["time"].as<long>();
+  const char* sensor = doc["sensor"];
+  long time = doc["time"];
+  double latitude = doc["data"][0];
+  double longitude = doc["data"][1];
+
+  // Print values.
+  Serial.println(sensor);
+  Serial.println(time);
+  Serial.println(latitude, 6);
+  Serial.println(longitude, 6);
 }
+
+
+// *********************************************************** Kvizzy30.ino ***
---
+++ Kvizzy30/Kvizzy30/common_kvizzy.h
@@ -0,0 +1,62 @@
+/** Arduino, Esp32-CAM ************************************ common_kvizzy.h ***
+ * 
+ * Определить общие функции нижнего уровня умного хозяйства на двух светодиодах
+ * 
+ * v1.0, 26.10.2024                                   Автор:      Труфанов В.Е.
+ * Copyright © 2024 tve                               Дата создания: 26.10.2024
+**/
+
+#ifndef common_kvizzy
+#define common_kvizzy
+#pragma once            
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+// ****************************************************************************
+// *                      Преобразовать json-документ в строку                *
+// *                             https://arduinojson.org/                     *
+// ****************************************************************************
+String jison1()
+{
+   String str = "***";
+   // Allocate the JSON document
+   JsonDocument doc;
+   // Add values in the document
+   doc["sensor"] = "gps";
+   doc["time"] = 1351824120;
+   // Add an array.
+   JsonArray data = doc["data"].to<JsonArray>();
+   data.add(48.756080);
+   data.add(2.302038);
+
+   serializeJson(doc,str);
+   //serializeJsonPretty(doc,str);
+
+   /*
+   // Generate the minified JSON and send it to the Serial port.
+   serializeJson(doc, Serial);
+   // The above line prints:
+   // {"sensor":"gps","time":1351824120,"data":[48.756080,2.302038]}
+   // Start a new line
+   Serial.println();
+
+   // Generate the prettified JSON and send it to the Serial port.
+   //serializeJsonPretty(doc, Serial);
+   // The above line prints:
+   // {
+   //   "sensor": "gps",
+   //   "time": 1351824120,
+   //   "data": [
+   //     48.756080,
+   //     2.302038
+   //   ]
+   // }
+   */
+
+   return str;
+}
+
+#endif
+
+// ******************************************************** common_kvizzy.h ***
