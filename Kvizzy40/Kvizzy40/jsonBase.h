c615287deedd00efee504a35a9984f9ab010a3a3
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-04-25 07:45:18 +0300

    Update jsonBase.h

--- Kvizzy40/Kvizzy40/jsonBase.h
+++ Kvizzy40/Kvizzy40/jsonBase.h
@@ -11,9 +11,14 @@
 #include <ArduinoJson.h>
 
 // Реестр json-сообщений на страницу State
-const String s4_HIGH  = "{\"led4\":[{\"status\":\"shimHIGH\"}]}";  // "вспышка включена"
-const String s4_LOW   = "{\"led4\":[{\"status\":\"shimLOW\"}]}";   // "вспышка ВЫКЛЮЧЕНА"
-const String s4_MODE0 = "{\"led4\":[{\"regim\":0}]}";              // "режим работы вспышки отключен"
+const String s4_HIGH    = "{\"led4\":[{\"status\":\"shimHIGH\"}]}";                 // "вспышка включена"
+const String s4_LOW     = "{\"led4\":[{\"status\":\"shimLOW\"}]}";                  // "вспышка ВЫКЛЮЧЕНА"
+const String s4_MODE0   = "{\"led4\":[{\"regim\":0}]}";                             // "режим работы вспышки отключен"
+const String s4_MODEini = "{\"led4\":[{\"regim\":1,\"light\":10,\"time\":2000}]}";  // "начальный режим работы вспышки"
+// Назначаем действующий режим работы вспышки
+int jlight=10;   // процент времени свечения в цикле 
+int jtime=2000;  // длительность цикла "горит - не горит" (мсек)      
+String s4_MODE = "{\"led4\":[{\"regim\":1,\"light\":"+String(jlight)+",\"time\":"+String(jtime)"+"}]}";  
 
 String jempty = "{}";  // пустая json-строка
 String sjson;          // выборка из json-документа
