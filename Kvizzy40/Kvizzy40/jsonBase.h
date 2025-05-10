573f7277f88273b7b48cdd5d3465184a284ee5d9
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-05-10 08:00:10 +0300

    Обработка ответа на запрос изменений

--- Kvizzy40/Kvizzy40/jsonBase.h
+++ Kvizzy40/Kvizzy40/jsonBase.h
@@ -10,34 +10,36 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
+// --- устарело
 // Реестр json-сообщений на страницу State
 const String s4_HIGH    = "{\"led4\":[{\"status\":\"shimHIGH\"}]}";                 // "вспышка включена"
 const String s4_LOW     = "{\"led4\":[{\"status\":\"shimLOW\"}]}";                  // "вспышка ВЫКЛЮЧЕНА"
 const String s4_MODE0   = "{\"led4\":[{\"regim\":0}]}";                             // "режим работы вспышки отключен"
 const String s4_MODEini = "{\"led4\":[{\"regim\":1,\"light\":10,\"time\":2000}]}";  // "начальный режим работы вспышки"
+// --- устарело
 
 // Назначаем параметры действующего режима работы вспышки
-bool Led4Start=true;     // true - включить режим работы вспышки
+bool Led4Start=false;    // true - изменён режим работы вспышки
 int jlight=10;           // процент времени свечения в цикле 
 int jtime=2000;          // длительность цикла "горит - не горит" (мсек)      
 int nLight=5000000;      // время (5 сек) свечения в цикле, далее пересчитывается 
 int nNoLight=5000000;    // время (5 сек) НЕ свечения в цикле, далее пересчитывается  
 
-String s4_MODE = "{\"led4\":[{\"regim\":1,\"light\":"+String(jlight)+",\"time\":"+String(jtime)+"}]}";  
-
 // Назначаем интервалы подачи сообщений от контроллера 
-int jmode4=  7007;  // режим работы Led4 
-int jimg=    1001;  // подача изображения 
-int jtempvl= 3003;  // температура и влажность 
-int jlumin=  2002;  // освещённость камеры
-int jbar=    5005;  // атмосферное давление
+bool intrvStart=false;   // true - изменены интервалы отправки сообщений
+int jmode4=  7007;       // режим работы Led4 
+int jimg=    1001;       // подача изображения 
+int jtempvl= 3003;       // температура и влажность 
+int jlumin=  2002;       // освещённость камеры
+int jbar=    5005;       // атмосферное давление
 
 // Запрос изменений - {\"common\":0} 
 const String s_COMMON = "{\"common\":0}"; 
-// Режим работы вспышки - {\"led4\":[{\"light\":10,\"time\":2000}]}                                    
-String s_MODE4 = "{\"led4\":[{\"light\":"+String(jlight)+",\"time\":"+String(jtime)+"}]}"; 
-// Интервалы подачи сообщений от контроллера - {\"intrv\":[{\"mode4\":7007,\"img\":1001,\"tempvl\":3003,\"lumin\":2002,\"bar\":5005}]}
-String s_INTRV = "{\"intrv\":[{\"mode4\":"+String(jmode4)+",\"img\":"+String(jimg)+",\"tempvl\":"+String(jtempvl)+",\"lumin\":"+String(jlumin)+",\"bar\":"+String(jbar)+"}]}"; 
+// Режим работы вспышки - "{\"led4\":{\"light\":10,\"time\":2000}}"                                    
+String s_MODE4 = "{\"led4\":{\"light\":"+String(jlight)+",\"time\":"+String(jtime)+"}}"; 
+String s4_MODE = "{\"led4\":[{\"regim\":1,\"light\":"+String(jlight)+",\"time\":"+String(jtime)+"}]}";  // ---устарело
+// Интервалы подачи сообщений от контроллера - "{\"intrv\":{\"mode4\":7007,\"img\":1001,\"tempvl\":3003,\"lumin\":2002,\"bar\":5005}}"
+String s_INTRV = "{\"intrv\":{\"mode4\":"+String(jmode4)+",\"img\":"+String(jimg)+",\"tempvl\":"+String(jtempvl)+",\"lumin\":"+String(jlumin)+",\"bar\":"+String(jbar)+"}}"; 
 
 // ****************************************************************************
 // *        Инициировать параметры действующего режима работы вспышки         *
--- Kvizzy40/Kvizzy40/kviLead.h
+++ Kvizzy40/Kvizzy40/kviLead.h
@@ -18,154 +18,65 @@
 void match_callback(const char * match,const unsigned int length,const MatchState & ms)
 {
   // Выбираем очередной найденный фрагмент
-  // {"led33":[{"regim":0}]}
+  // {"led4":{"light":25,"time":1996},"intrv":{"mode4":6900,"img":1001,"tempvl":3003,"lumin":2002,"bar":5005}}
   String sjson = String(match);
   sjson = sjson.substring(0,length);
-  Serial.print("sjson: "); Serial.println(sjson); 
-
-  //JsonDocument doc;
-  //deserializeJson(doc, sjson);
-
-  /*
-  // {"lead":[{"led4":[{"light":25,"time":1996}]},{"intrv":[{"mode4":6900,"img":1001,"tempvl":3003,"lumin":2002,"bar":5005}]}]}
-
-  String lead=doc["lead"];
-  Serial.print("lead: "); Serial.println(lead);
-  
-  // lead: [{"led4":[{"light":25,"time":1996}]},{"intrv":[{"mode4":6900,"img":1001,"tempvl":3003,"lumin":2002,"bar":5005}]}]
-  String led4=doc["lead"][0]["led4"];
-  Serial.print("led4: "); Serial.println(led4);
-  // led4: [{"light":25,"time":1996}]
-  int light = doc["lead"][0]["led4"][0]["light"];
-  Serial.print("light: "); Serial.println(light);
-
-  // lead: [{"led4":[{"light":25,"time":1996}]},{"intrv":[{"mode4":6900,"img":1001,"tempvl":3003,"lumin":2002,"bar":5005}]}]
-  String intrv=doc["lead"][1]["intrv"];
-  Serial.print("intrv: "); Serial.println(intrv);
-
-  //int mode4 = doc["lead"][0]["intrv"][0]["mode4"];
-  //Serial.print("mode4: "); Serial.println(mode4);
-  */
-
-  // {"led4":{"light":25,"time":1996},"intrv":{"mode4":6900,"img":1001,"tempvl":3003,"lumin":2002,"bar":5005}}
-  //sjson = "{\"led4\":{\"light\":25,\"time\":1996},\"intrv\":{\"mode4\":6900,\"img\":1001,\"tempvl\":3003,\"lumin\":2002,\"bar\":5005}}";
-  //Serial.print("sjson: "); Serial.println(sjson); 
+  // Serial.print("sjson: "); Serial.println(sjson); 
 
   JsonDocument doc;
   deserializeJson(doc, sjson);
 
+  // Определяемся и обрабатываем команду по режиму вспышки
   String led4=doc["led4"];
-  Serial.print("led4: "); Serial.println(led4);
-  int light = doc["led4"]["light"];
-  Serial.print("light: "); Serial.println(light);
-
+  //Serial.print("led4: "); Serial.println(led4);
+  //Serial.print("led4.length(): "); Serial.println(led4.length());
+  if (led4 != "null")
+  {
+    int light = doc["led4"]["light"];
+    Serial.print("light: "); Serial.println(light);
+    int time = doc["led4"]["time"];
+    Serial.print("time: "); Serial.println(time);
+  }
+  // Определяемся и обрабатываем команду по интервалам сообщений
   String intrv=doc["intrv"];
-  Serial.print("intrv: "); Serial.println(intrv);
-  int mode4 = doc["intrv"]["mode4"];
-  Serial.print("mode4: "); Serial.println(mode4);
+  if (intrv != "null")
+  {
+    //Serial.print("intrv: "); Serial.println(intrv);
+    int mode4  = doc["intrv"]["mode4"];
+    Serial.print("mode4: "); Serial.println(mode4);
+    int img    = doc["intrv"]["img"];
+    Serial.print("img: "); Serial.println(img);
+    int tempvl = doc["intrv"]["tempvl"];
+    Serial.print("tempvl: "); Serial.println(tempvl);
+    int lumin  = doc["intrv"]["lumin"];
+    Serial.print("lumin: "); Serial.println(lumin);
+    int bar    = doc["intrv"]["bar"];
+    Serial.print("bar: "); Serial.println(bar);
+  }
 
-  String vintrv = doc["vintrv"];
-  Serial.print("vintrv: "); Serial.println(vintrv);
-
- 
-   // Обрабатываем очередной найденный фрагмент
-   
-   // "режим контрольного светодиода выключен"
-   /*
-   if (sjson==s33_MODE0) 
-   {
-      Serial.println(); 
-      Serial.println(sjson); 
-      Serial.println("Выключаем режим контрольного светодиода"); 
-      Serial.println("Обновляем json-документ"); 
-      
-      // Отправляем сообщение на State
-      String inMess=queState.Send(s33_MODE0);
-      if (inMess!=tisOk) queMessa.Send(tmt_WARNING,NoSend33_MODE0,tmk_Queue);
-   }
-   else if (sjson==s33_LOW) 
-   { 
-      Serial.println(); 
-      Serial.print(sjson); Serial.println(" -> match_callback.ELSE-IF"); 
-   }
-   else 
-   { 
-      Serial.println(); 
-      Serial.print(sjson); Serial.println(" -> match_callback.ELSE"); 
-   }
-   */
+  //String vintrv=doc["vintrv"];
+  //Serial.print("vintrv: "); Serial.println(vintrv);
+  //Serial.print("vintrv.length(): "); Serial.println(vintrv.length());
 }
-// ****************************************************************************
-// *      Выполнить выборку всех json-сообщений из ответа страницы Lead       *
-// ****************************************************************************
-void getJsonLead(String httpText)
-{
-  unsigned long count;
-  // Загружаем в буфер текст, в котором будет осуществляться поиск
-  char buf[100];
-  count=httpText.length();
-  if (count+1>100) Serial.println("Ошибка, httpText превышает 100 символов!");
-  httpText.toCharArray(buf,count+1);  
-  // Создаем объект поиска соответствий
-  MatchState ms(buf);
-  // Формируем регулярное выражение ("соответствие") для поиска json-сообщений:
-  // "<Lead><p>{"led33":[{"regim":0}]}</p></Lead>"
-  const char * match="{[{\"a-z0-9:%[}%]]*}";
-  // Выполняем поиск по соответствию
-  count = ms.GlobalMatch(match,match_callback);
-}
-
 // ****************************************************************************
 // *      Выполнить выборку всех json-сообщений из ответа страницы Lead       *
 // ****************************************************************************
 void Deser(String httpText)
 {
-
-  
-  
-  
-  
-  
-  // Готовим ответ для парс
+  // Преобразовываем String-ответ в chars-ответ для парсинга
   int str_len = httpText.length()+1; 
   char json[str_len];
   httpText.toCharArray(json, str_len);
   // Трассируем ответ от Lead
-  Serial.print("json: "); Serial.println(json);
-
+  // Serial.print("json: "); Serial.println(json);
   // Создаем объект поиска соответствий
   MatchState ms(json);
   // Формируем регулярное выражение ("соответствие") для поиска json-сообщений:
-  // <Lead>{"lead":[{"led4":[{"light":25,"time":1996}]},{"intrv":[{"mode4":6900,"img":1001,"tempvl":3003,"lumin":2002,"bar":5005}]}]}</Lead>
+  // <Lead>{"led4":{"light":25,"time":1996},"intrv":{"mode4":6900,"img":1001,"tempvl":3003,"lumin":2002,"bar":5005}}</Lead>
   const char * match="{[{\"a-z0-9:,%[}%]]*}";
   // Выполняем поиск по соответствию
   int count = ms.GlobalMatch(match,match_callback);
-  Serial.print("count: "); Serial.println(count);
-
-
-  /*
-  charBuf[stringOne.length()+1] –
-  
-  char json[512] = httpText;
-  JsonDocument doc;
-//deserializeJson(doc, json);
-*/
-
-
-/*
-char json[] = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
-
-JsonDocument doc;
-deserializeJson(doc, json);
-
-const char* sensor = doc["sensor"];
-long time          = doc["time"];
-double latitude    = doc["data"][0];
-double longitude   = doc["data"][1];
-*/
-
-
-
+  // Serial.print("count: "); Serial.println(count);
 }
 
 // * Задача FreRTOS ***********************************************************
@@ -190,9 +101,6 @@ void vLead(void* pvParameters)
         // принятого в последовательном порту
         if (iCreateSit == loopingLed4) MimicMCUhangEvent("Led4");   
         */
-        // Передаём запрос на изменение состояний
-        Led4State(s4_MODE);
-
         // Готовим запрос к странице Lead
         String ehttp=urlHome+"/Lead40/";
         // Изменяем значение счетчика и включаем его в параметр запроса к странице State
@@ -200,8 +108,22 @@ void vLead(void* pvParameters)
         String queryString = "cycle="+String(iLead);    
         // Готовим структуру для ответа
         tQueryMessage tQuery;                              
-        // Включаем в параметр запроса json-сообщение
+
+        // По умолчанию включаем в параметр запрос изменений
         String sjson="&sjson="+s_COMMON;
+        // Если изменен режим работы вспышки
+        if (Led4Start) 
+        {
+          s_MODE4 = "{\"led4\":{\"light\":"+String(jlight)+",\"time\":"+String(jtime)+"}}"; 
+          sjson="&sjson="+s_MODE4;
+        }
+        // Если изменены интервалы отправки сообщений
+        else if (intrvStart) 
+        {
+          s_INTRV = "{\"intrv\":{\"mode4\":"+String(jmode4)+",\"img\":"+String(jimg)+",\"tempvl\":"+String(jtempvl)+",\"lumin\":"+String(jlumin)+",\"bar\":"+String(jbar)+"}}"; 
+          sjson="&sjson="+s_INTRV;
+        }
+
         queryString=queryString+sjson;
         tQuery = postQuery(ehttp, queryString);
         // Обрабатываем успешный запрос 
